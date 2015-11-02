cd2527ceb4cb7e5d1f3e9632e3e82b9b418a3926
subbarker <subbarker@gmail.com>
2015-11-02 05:26:48 -0600

    beginnings of unittest, problems with imports

--- test.py
+++ test.py
@@ -1,25 +1,46 @@
-import json
-import requests
-headers = {'content-type': 'application/json'}
+# import json
+# import requests
+# headers = {'content-type': 'application/json'}
+#
+# t1 = requests.post('http://127.0.0.1:5000/v1/tournaments/').text
+# print(t1)
+# t2 = requests.post('http://127.0.0.1:5000/v1/tournaments/').text
+# print(t2)
+#
+# names = ['BYE', 'jeff', 'shirley', 'britta', 'abed']
+#
+# players = [requests.post('http://127.0.0.1:5000/v1/players/', headers=headers, data=json.dumps({'name': n})).text for n in names]
+# print(players)
+#
+# requests.post(t1 + '/players/', headers=headers, data=json.dumps({'id': 1}))
+# requests.post(t1 + '/players/', headers=headers, data=json.dumps({'id': 2}))
+# requests.post(t1 + '/players/', headers=headers, data=json.dumps({'id': 3}))
+# requests.post(t1 + '/players/', headers=headers, data=json.dumps({'id': 4}))
+# requests.post(t1 + '/players/', headers=headers, data=json.dumps({'id': 5}))
+#
+#
+# print(requests.post('http://127.0.0.1:5000/v1/matches/', headers=headers, data=json.dumps({'tournament_id': 1, 'player_1_id': 2, 'player_2_id': 3})).text)
+# print(requests.post('http://127.0.0.1:5000/v1/matches/').text)
+#
+# requests.put('http://127.0.0.1:5000/v1/matches/2', headers=headers, data=json.dumps({'player_1_id': 4}))
 
-t1 = requests.post('http://127.0.0.1:5000/v1/tournaments/').text
-print(t1)
-t2 = requests.post('http://127.0.0.1:5000/v1/tournaments/').text
-print(t2)
 
-names = ['BYE', 'jeff', 'shirley', 'britta', 'abed']
-
-players = [requests.post('http://127.0.0.1:5000/v1/players/', headers=headers, data=json.dumps({'name': n})).text for n in names]
-print(players)
-
-requests.post(t1 + '/players/', headers=headers, data=json.dumps({'id': 1}))
-requests.post(t1 + '/players/', headers=headers, data=json.dumps({'id': 2}))
-requests.post(t1 + '/players/', headers=headers, data=json.dumps({'id': 3}))
-requests.post(t1 + '/players/', headers=headers, data=json.dumps({'id': 4}))
-requests.post(t1 + '/players/', headers=headers, data=json.dumps({'id': 5}))
+import os
+import tournament
+import unittest
+import tempfile
 
 
-print(requests.post('http://127.0.0.1:5000/v1/matches/', headers=headers, data=json.dumps({'tournament_id': 1, 'player_1_id': 2, 'player_2_id': 3})).text)
-print(requests.post('http://127.0.0.1:5000/v1/matches/').text)
+class TournamentTestCase(unittest.TestCase):
 
-requests.put('http://127.0.0.1:5000/v1/matches/2', headers=headers, data=json.dumps({'player_1_id': 4}))
+    def setUp(self):
+        self.db_fd, tournament.app.config['DATABASE'] = tempfile.mkstemp()
+        tournament.app.config['TESTING'] = True
+        self.app = tournament.app.test_client()
+
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink(tournament.app.config['DATABASE'])
+
+if __name__ == '__main__':
+    unittest.main()
